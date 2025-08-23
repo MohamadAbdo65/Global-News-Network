3b015a103434322866179b9028acf19802771575
محمد <www.modi65@gmail.com>
2025-08-23 14:53:18 +0300

    Setup enviroment

---
+++ GNN_DAL/Repos/AdminRepo.cs
@@ -0,0 +1,20 @@
+﻿using GNN_DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GNN_DAL.Repos
+{
+    public class AdminRepo
+    {
+        private readonly GlobalNewsNetworkContext _context;
+
+        public AdminRepo(GlobalNewsNetworkContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
